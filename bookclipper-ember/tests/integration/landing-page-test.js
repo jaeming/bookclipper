3353b4c9a35ab5805f817ec9b7b14e70d29ca1f1
Benji Dalton <daylightsavings@gmail.com>
2014-12-24 05:44:28 +1300

    starts integration testing

---
+++ bookclipper-ember/tests/integration/landing-page-test.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import startApp from 'bookclipper/tests/helpers/start-app';
+import Ember from 'ember';
+
+var App;
+
+module('Integration - Landing Page', {
+  setup: function() {
+    App = startApp();
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test('Should welcome me to Bookclipper', function() {
+  visit('/').then(function() {
+    equal(find('h2#title').text(), 'Welcome to Bookclipper');
+  });
+});

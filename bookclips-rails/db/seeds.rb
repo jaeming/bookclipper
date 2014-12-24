2b7b724d9399dfec4444d907b65d7537dccd54aa
Benji Dalton <daylightsavings@gmail.com>
2014-12-25 06:54:33 +1300

    adds seed data for bookmarks

--- bookclips-rails/db/seeds.rb
+++ bookclips-rails/db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+20.times do
+  bookmark = Bookmark.new(
+  title:       Faker::Internet.domain_word,
+  description: Faker::Lorem.sentence,
+  url:  Faker::Internet.url,
+  tag:  Faker::Lorem.word
+  )
+  bookmark.save!
+end
+
+puts "Seeds generated"
+puts "#{Bookmark.count} Bookmarks created"

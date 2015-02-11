200cb0543c1561cf941582bca280f1f0048d9e67
Benji Dalton <daylightsavings@gmail.com>
2015-02-12 02:33:01 +1300

    Workaround fix for serialization_scope bug from active_record_serializer

--- bookclips-rails/app/controllers/application_controller.rb
+++ bookclips-rails/app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
+  include ActionController::Serialization
   respond_to :html, :json
   skip_before_action :verify_authenticity_token
   # protect_from_forgery with: :null_session
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   serialization_scope :view_context
+
   protected
 
     def configure_permitted_parameters

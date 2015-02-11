e81f6b7a9b4ff96a09bd4e2c23cdc6740f000048
Benji Dalton <daylightsavings@gmail.com>
2015-02-12 01:56:23 +1300

    cleans up unnecessary files for Rails

--- bookclips-rails/app/serializers/bookmark_serializer.rb
+++ bookclips-rails/app/serializers/bookmark_serializer.rb
@@ -1,7 +1,5 @@
 class BookmarkSerializer < ActiveModel::Serializer
   attributes :id, :url, :title, :description, :tags, :hashtags, :users, :authenticated
-  # has_many :hashtags, embed: :ids, include: true
-  # has_many :users, embed: :ids, include: true
   delegate :current_user, to: :scope
 
   def hashtags
